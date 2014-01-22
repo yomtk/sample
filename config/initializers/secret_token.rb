4649cabfac124091ed283540bb8826ecaaa35449
yomtk <yosmtok@gmail.com>
2014-01-22 17:52:58 +0900

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleProject::Application.config.secret_key_base = 'ae6226137a2c341b9ba25ff38721bb37e0ebf5de726eb23199b97bef8727d85d60ff05e668b2a03949cf15ccd08efef592eba8d28a8c664e1c24f7ab706d5729'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleProject::Application.config.session_store :cookie_store, key: '_sample_project_session'
